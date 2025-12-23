0309cd75f2b2dac1e178ff9f23ee1f33cc3cf602
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2025-12-23 18:10:02 +0900

    0383-RANSOM-NOTE Stats: Time: 36 ms (46.79%), Space: 12.7 MB (51.96%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
+        counts = [0] * 26
+
+        for char in magazine:
+            counts[ord(char) - ord('a')] += 1
+
         for char in ransomNote:
-            if char in magazine:
-                magazine = magazine.replace(char, "", 1)
-            else:
+            index = ord(char) - ord('a')
+
+            if counts[index] == 0:
                 return False
-        
+            
+            counts[index] -= 1
+
         return True

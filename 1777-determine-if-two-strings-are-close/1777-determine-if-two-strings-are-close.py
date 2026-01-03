9cb177a65515d54de30fdf7a3b157091606a4db1
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-03 17:35:41 +0900

    1777-DETERMINE-IF-TWO-STRINGS-ARE-CLOSE Stats: Time: 77 ms (96.78%), Space: 12.9 MB (99.71%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -2,19 +2,10 @@ class Solution(object):
     def closeStrings(self, word1, word2):
         if set(word1) != set(word2):
             return False
-
-        freq1 = {}
-        freq2 = {}
-
-        for i in word1:
-            freq1[i] = freq1.get(i, 0) + 1
         
-        for i in word2:
-            freq2[i] = freq2.get(i, 0) + 1
-        
-        # freq1 = sorted([word1.count(i) for i in set(word1)])
-        # freq2 = sorted([word2.count(i) for i in set(word2)])
-        # return freq1 == freq2
+        freq1 = sorted([word1.count(i) for i in set(word1)])
+        freq2 = sorted([word2.count(i) for i in set(word2)])
+        return freq1 == freq2
 
-        return sorted(freq1.values()) == sorted(freq2.values())
+
         

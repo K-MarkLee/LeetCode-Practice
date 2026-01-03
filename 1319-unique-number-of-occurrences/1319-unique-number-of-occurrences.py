c88b0247ab6907813292f3b27e4363cacc034ff4
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-03 16:39:16 +0900

    1319-UNIQUE-NUMBER-OF-OCCURRENCES Stats: Time: 0 ms (100%), Space: 12.4 MB (99.21%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -5,9 +5,12 @@ class Solution(object):
         for i in arr:
             result[i] = result.get(i, 0) + 1
 
-        setarr = set()
-        for i in result.values():
-            if i in setarr:
-                return False
-            setarr.add(i)
-        return True
+        # setarr = set()
+        # for i in result.values():
+        #     if i in setarr:
+        #         return False
+        #     setarr.add(i)
+        # return True
+        
+        values = result.values()
+        return len(values) == len(set(values))

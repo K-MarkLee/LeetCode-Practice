e79201aa27e63ef3dc0c00ad7c6e2c82ddc785ff
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-03 12:37:01 +0900

    1392-FIND-THE-DIFFERENCE-OF-TWO-ARRAYS Stats: Time: 1 ms (95.53%), Space: 12.8 MB (25.82%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
@@ -3,6 +3,4 @@ class Solution(object):
         set1 = set(nums1)
         set2 = set(nums2)
 
-        result1 = list(set1 - set2)
-        result2 = list(set2 - set1)
-        return[result1, result2]
+        return [list(set1 - set2), list(set2 - set1)]

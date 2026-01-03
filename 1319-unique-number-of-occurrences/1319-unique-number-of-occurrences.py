7539ace3d303c1cb58e7bd467f076159297de533
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-03 15:59:10 +0900

    1319-UNIQUE-NUMBER-OF-OCCURRENCES Stats: Time: 0 ms (100%), Space: 12.4 MB (80.96%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        result = {}
+
+        for i in arr:
+            result[i] = result.get(i, 0) + 1
+
+        setarr = set()
+        for i in result.values():
+            if i in setarr:
+                return False
+            setarr.add(i)
+        return True

02ecc59358d805e6153eb1afa9e3ddc4db9852a4
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-05 15:04:10 +0900

    2428-EQUAL-ROW-AND-COLUMN-PAIRS Stats: Time: 29 ms (85.29%), Space: 15.7 MB (64.62%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -1,19 +1,15 @@
 class Solution(object):
     def equalPairs(self, grid):
         n = len(grid)
-        result = 0
         row = {}
-        col = {}
+        result = 0
 
         for i in range(n):
             key = tuple(grid[i])
             row[key] = row.get(key, 0) + 1
+
         for j in range(n):
-            ckey = tuple(grid[i][j] for i in range(n))
-            col[ckey] = col.get(ckey, 0) + 1
-        
-        for key in row:
-            if key in col:
-                result += row[key] * col[key]
+            key = tuple(grid[i][j] for i in range(n))
+            result += row.get(key, 0)
         
         return result

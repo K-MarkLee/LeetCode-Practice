527c64ad84e3993f931c20cff3962ff1a5ef87d0
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-25 16:47:31 +0900

    1544-COUNT-GOOD-NODES-IN-BINARY-TREE Stats: Time: 206 ms (80.54%), Space: 48.2 MB (80%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def goodNodes(self, root):
         """
@@ -18,11 +12,10 @@ class Solution(object):
 
             if node.val >= maximum:
                 good += 1
-                maximum = node.val
 
             if node.right:
-                stack.append((node.right, max(maximum, node.right.val)))
+                stack.append((node.right, max(maximum, node.val)))
             if node.left:
-                stack.append((node.left, max(maximum, node.left.val)))
+                stack.append((node.left, max(maximum, node.val)))
 
         return good

92a3315d5369a9fc37537d1d4c8f762cf718d1a8
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-25 16:45:05 +0900

    1544-COUNT-GOOD-NODES-IN-BINARY-TREE Stats: Time: 257 ms (8.93%), Space: 48.3 MB (80%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -10,19 +10,19 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        maximum = root.val
-        stack = [(root, maximum)]
-        good = []
+        stack = [(root, root.val)]
+        good = 0
 
         while stack:
             node, maximum = stack.pop()
 
             if node.val >= maximum:
-                good.append(node.val)
+                good += 1
+                maximum = node.val
 
             if node.right:
-                stack.append((node.right, max(maximum, node.val)))
+                stack.append((node.right, max(maximum, node.right.val)))
             if node.left:
-                stack.append((node.left, max(maximum, node.val)))
+                stack.append((node.left, max(maximum, node.left.val)))
 
-        return len(good)
+        return good
